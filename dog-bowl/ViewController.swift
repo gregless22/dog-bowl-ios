b767a38e49aefeb9e47aadcbbab44283ecccaaa4
gregless <gregoryconnolly@gmail.com>
2018-06-16 12:07:09 +1000

    Initial Commit

---
+++ dog-bowl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dog-bowl
+//
+//  Created by iMac on 16/6/18.
+//  Copyright © 2018 sunny-coast-coders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
