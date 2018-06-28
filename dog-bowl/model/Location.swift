0651ef56ca72896c817649ce8469a0908f9cd1fc
Greg Connolly <gregoryconnolly@gmail.com>
2018-06-28 14:01:25 +1000

    Add some story board and files

---
+++ dog-bowl/model/Location.swift
@@ -0,0 +1,9 @@
+//
+//  Location.swift
+//  dog-bowl
+//
+//  Created by Greg Connolly on 28/6/18.
+//  Copyright © 2018 sunny-coast-coders. All rights reserved.
+//
+
+import Foundation
---
+++ dog-bowl/model/model.swift
@@ -0,0 +1,11 @@
+//
+//  model.swift
+//  dog-bowl
+//  this swift file is for the implementation of the model. It is for ensuring that it is
+//  a singelton and there is only one instance of the class.
+//
+//  Created by Greg Connolly on 28/6/18.
+//  Copyright © 2018 sunny-coast-coders. All rights reserved.
+//
+
+import Foundation
